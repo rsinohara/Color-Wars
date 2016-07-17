b0090dd09a48750cfbab58ae771b9b02abf36535
rsinohara <rsinohara@hotmail.com>
2016-07-17 17:14:10 +0200

    GridInitializeTest

--- Core/gameGrid.js
+++ Core/gameGrid.js
@@ -1,1 +1,18 @@
-﻿alert('a');
+﻿
+class gameGrid {
+
+    initializeGrid(width, height) {
+        this.grid = Array();
+        for(var i=0;i<width;i++)
+        {
+            this.grid[i] = Array();
+            for (var j = 0; j < height; j++) {
+                var cell = new gameCell();
+                cell.initRandom();
+                this.grid[i][j] = cell;
+            }
+        }
+    }
+
+
+}
