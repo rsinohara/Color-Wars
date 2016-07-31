92baed3f54cf16a09ebfe41bdc0693666a07b797
Rebeca <rebeca.abrantes@gmail.com>
2016-07-31 22:49:56 +0200

    changed grid colors

--- Core/gameCell.js
+++ Core/gameCell.js
@@ -6,8 +6,6 @@ class gameCell {
 
 
     initRandom() {
-        var colorTable = ["red", "orange", "yellow", "green", "blue", "purple", "white", "pink", "brown"];
-
         this.color = Math.floor(Math.random() * 9) ;
         this.owner = 0;
         this.surrounded = false;
--- Core/utilities.js
+++ Core/utilities.js
@@ -1,9 +1,12 @@
 ﻿"use strict";
-var colorTable = ["red", "orange", "yellow", "green", "blue", "purple", "white", "pink", "brown"];
+//var colorTable = ["#ED1B51", "#FAA21B", "#F7EC2F", "#5DBB4D", "#51A6DC", "#83769C", "#F176A6", "#FCCCAB", "brown"];
+//var colorTable = ["#FE0000", "#FF9C00", "#F5FF00", "#3CF10E", "#01FFE5", "#0024FF", "#C500FF", "#FF009C", "brown"];
+//var colorTable = ["#FEFF35", "#FC9A03", "#FF2710", "#B0019E", "#3E01A4", "#0248FF", "#66B132", "#D0EB2C", "brown"];
+var colorTable = ["red", "orange", "yellow", "#0F9200", "#02B8D8", "#86269B", "pink", "#4AE54A", "#CC0063"];
 
 function getColorString(color)
 {
-     
+
 
     return colorTable[color];
 }
